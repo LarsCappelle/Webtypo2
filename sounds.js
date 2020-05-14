481caba4ad12aa157022797d0b19610629db5b13
LarsCappelle <63396655+LarsCappelle@users.noreply.github.com>
2020-05-14 13:21:03 +0200

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,16 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0,      //class sound0
+    2,      //class sound1
+	9,      //class sound2
+	12,     //class sound3
+	14,     //class sound4
+	15,        //class sound5
+	16,        //class sound6
+	25.8,   //class sound7
+	31,        //class sound8
+	32.8,        //class sound9
+	68,        //class sound10
+	75         //class sound11
+];
